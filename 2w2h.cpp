5c5806b44b3f187fdc00aaf025a442d005e18007
wsc0306 <wsc63216321@gmail.com>
2026-03-11 23:26:18 +0900

    Add files via upload

---
+++ 2w2h.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main()
+{
+     std::cout << "I LOVE C++" <<  std::endl;
+     std::cout <<  std::endl;
+
+    std::cout << "I       L        OOOOO    V           V     EEEEE" <<  std::endl;
+    std::cout << "I       L       O     O    V         V      E"     <<  std::endl;
+    std::cout << "I       L       O     O     V       V       E"     <<  std::endl;
+    std::cout << "I       L       O     O      V     V        EEEE"  <<  std::endl;
+    std::cout << "I       L       O     O       V   V         E"     <<  std::endl;
+    std::cout << "I       L       O     O        V V          E"     <<  std::endl;
+    std::cout << "I       LLLLL    OOOOO          V           EEEEE" <<  std::endl;
+
+    std::cout <<  std::endl;
+
+    std::cout << "  CCCCC        ++        ++"    <<  std::endl;
+    std::cout << " C             ++        ++"    <<  std::endl;
+    std::cout << " C             ++        ++"    <<  std::endl;
+    std::cout << " C          ++++++++  ++++++++" <<  std::endl;
+    std::cout << " C             ++        ++"    << std::endl;
+    std::cout << " C             ++        ++"    <<  std::endl;
+    std::cout << "  CCCCC        ++        ++"    << std::endl;
+
+    return 0;
+}
